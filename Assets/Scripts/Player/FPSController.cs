3d2818ac4a8b35b684ae6fe224d0bc4cfc858d11
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-10-19 16:11:45 -0300

    Started fps controller script

---
+++ Assets/Scripts/Player/FPSController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+
+    
+}
