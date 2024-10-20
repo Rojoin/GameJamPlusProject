a16f3bf97782a92d9a8c12c86644e984301d86c8
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-10-20 17:38:02 -0300

    Main menu first impl

---
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        Debug.Log("Play");
+    }
+
+    public void OptionsButton()
+    {
+        Debug.Log("Options");
+    }
+
+    public void CreditsButton()
+    {
+        Debug.Log("Credits");
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quit");
+    }
+}
