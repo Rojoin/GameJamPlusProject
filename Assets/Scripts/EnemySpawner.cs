15c49adfb8649a0f8352a34a4ae2c3de045d9d74
Rojoin <nachoarrastua@gmail.com>
2024-10-21 01:32:40 -0300

    Add Way to go back to the menu

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,26 +2,35 @@ using Autodesk.Fbx;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<Transform> spawnLocations = new();
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private GameObjectChannelSO playerChannel;
+    [SerializeField] private VoidChannelSO playerDeathChannel;
 
     [SerializeField] private float maxTimer = 7.0f;
     [SerializeField] private Transform playerTransform;
 
     private float currentTimer = 0.0f;
 
-    private void Awake()
+    private void OnEnable()
     {
         playerChannel.Subscribe(AssignPlayer);
+        playerDeathChannel.Subscribe(GoToFirstScene);
     }
 
-    private void OnDestroy()
+    private void GoToFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnDisable()
     {
         playerChannel.Unsubscribe(AssignPlayer);
+        playerDeathChannel.Unsubscribe(GoToFirstScene);
     }
 
     private void Update()
@@ -38,7 +47,7 @@ public class EnemySpawner : MonoBehaviour
     {
         currentTimer += Time.deltaTime;
 
-        if(currentTimer > maxTimer)
+        if (currentTimer > maxTimer)
         {
             SpawnEnemy();
 
@@ -50,7 +59,7 @@ public class EnemySpawner : MonoBehaviour
     {
         int rnd = Random.Range(0, spawnLocations.Count);
 
-        var enemy = Instantiate(enemyPrefab,spawnLocations[rnd].position, spawnLocations[rnd].rotation);
+        var enemy = Instantiate(enemyPrefab, spawnLocations[rnd].position, spawnLocations[rnd].rotation);
         enemy.GetComponent<Enemy>().SetPlayer(playerTransform);
     }
 }
